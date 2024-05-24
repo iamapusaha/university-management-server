a65d4b6af90282627d8c0eea5ce11b96a7fb8ed2
iamapusaha <apusaha392@gmail.com>
2024-05-24 12:32:59 +0600

    update app.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("university server is running!");
+  res.send("university server is running on the highway!");
 });
 
 export default app;
