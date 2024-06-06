97efdfa2307d6da86bb5c5565d7b6d9f9b978bbe
iamapusaha <apusaha392@gmail.com>
2024-06-07 03:47:35 +0600

    added academic Faculty router

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import academicFacultyValidation from "./academicFaculty.validation";
 import { academicFacultyController } from "./academicFaculty.controller";
 import validateRequest from "../../middlewares/validateRequest";
+import { academicSemesterController } from "../academicSemester/academicSemester.controller";
 
 const router = Router();
 
@@ -11,3 +12,20 @@ router.post(
   validateRequest(academicFacultyValidation),
   academicFacultyController.createAcademicFaculty
 );
+
+router.get(
+  "/get-all-academic-faculty",
+  academicSemesterController.getAllAcademicSemesterData
+);
+
+router.get(
+  "/get-single-academic-faculty",
+  academicSemesterController.getSingleAcademicSemesterData
+);
+router.patch(
+  "/update-academic-faculty",
+  validateRequest(academicFacultyValidation),
+  academicSemesterController.updateAcademicSemester
+);
+
+export const academicFacultyRouters = router;
