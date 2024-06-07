eb41178213db28062a52d5c3e7a27db220ba8ea8
iamapusaha <apusaha392@gmail.com>
2024-06-07 23:24:05 +0600

    added catchAsyn for every controller on academic department

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -3,6 +3,12 @@ import { TAcademicDepartment } from "./academicDepartment.interface";
 import { AcademicDepartment } from "./academicDepartment.model";
 
 const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const isDepartmentExists = await AcademicDepartment.find({
+    name: payload.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error("Department is alrady exists!");
+  }
   const result = await AcademicDepartment.create(payload);
   return result;
 };
