8e6ae2b5cc92ede7bd8d3d068b1c55d20aa86ec2
iamapusaha <apusaha392@gmail.com>
2024-06-08 09:19:33 +0600

    added populate function to get ref value on depart services

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,12 +1,25 @@
 import Student from "./student.model";
 
 const getAllStudentFromDB = async () => {
-  const result = await Student.find();
+  const result = await Student.find()
+    .populate({
+      path: "academicDepartment",
+      populate: {
+        path: "academicFaculty",
+      },
+    })
+    .populate("admissionSemester");
   return result;
 };
 const getSingleStudentFromDB = async (id: string) => {
-  // const result = await Student.findOne({ id });
-  const result = await Student.aggregate([{ $match: { id: id } }]);
+  const result = await Student.findOne({ id })
+    .populate({
+      path: "academicDepartment",
+      populate: {
+        path: "academicFaculty",
+      },
+    })
+    .populate("admissionSemester");
   return result;
 };
 const deleteStudentFromDB = async (id: string) => {
