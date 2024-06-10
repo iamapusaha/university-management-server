68798625c4d24f9340a8fe241a10411d4cf05bbb
iamapusaha <apusaha392@gmail.com>
2024-06-10 11:29:27 +0600

    handle unhandleRejection error

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import router from "./app/router";
 import notFound from "./app/middlewares/notFound";
 import globalErrorHandlar from "./app/middlewares/globalErrorHandlar";
+import { promise } from "zod";
 const app: Application = express();
 
 //parsers
@@ -12,13 +13,13 @@ app.use(cors());
 
 app.use("/api/v1/", router);
 
+const test = (req: Request, res: Response) => {
+  res.send("university server is running on the highway!");
+};
+
+app.get("/", test);
+app.use(globalErrorHandlar);
 //not found route
 app.use(notFound);
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("university server is running on the highway!");
-});
-
-app.use(globalErrorHandlar);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import { Server } from "http";
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listing on port ${config.port}`);
     });
   } catch (error) {
@@ -13,3 +15,12 @@ async function main() {
   }
 }
 main();
+process.on("unhandledRejection", () => {
+  console.log("🥵 unhandleRejection is detected,shutting down!");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
